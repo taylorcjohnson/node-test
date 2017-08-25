0f6d870e043b3feec6f2ce58ae18469bf1047403
Taylor Johnson <taylor.christian.johnson@gmail.com>
2017-08-25 18:54:47 -0400

    Initial commit

---
+++ utils/utils.test.js
@@ -0,0 +1,46 @@
+// Command to run: npm test (redefined test script in package.json)
+// Continuous testing: nodemon --exec 'npm test'
+// Moved to 'npm run test-watch'
+const expect = require('expect')
+
+const utils = require('./utils')
+
+it('should add two numbers', () => {
+    let res = utils.add(33, 11)
+
+    expect(res).toBeA('number').toBe(44)
+})
+
+it('should square a number', () => {
+    let res = utils.square(10)
+
+    expect(res).toBeA('number').toBe(100)
+})
+
+it('should expect a value', () => {
+    //expect(12).toNotBe(11)
+    //expect({name: 'Taylor'}).toNotEqual({name: 'taylor'})
+    //expect([2,3,4]).toInclude(3)
+    expect({
+        name: 'Taylor',
+        age: 31,
+        location: 'Florida'
+    }).toExclude({
+        age: 32
+    })
+})
+
+it('should verify first and last names are set', () => {
+    let person = {
+        age: 31,
+        location: 'Florida',
+        firstName: 'Bob',
+        lastName: 'Boberts'
+    }
+    let res = utils.setName(person, 'Taylor Johnson')
+    
+    expect(res).toBeA('object').toInclude({
+        firstName: 'Taylor',
+        lastName: 'Johnson'
+    })
+})
